1b0bf0bdf7782ad9dd0e861dbf9e2765d7bb90c4
Leonidas Giannoulis <masterleo@gmail.com>
2026-02-06 21:36:18 +0200

    bulk

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
+  /*output:'export',*/
   /*assetPrefix: './',*/
   productionBrowserSourceMaps: true,
   distDir: process.env.DIST_DIR || '.next',  typescript: {
